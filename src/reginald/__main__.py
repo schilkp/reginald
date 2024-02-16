3271de6bf9e57a61f0a1704d5643d2c564302098
schilkp <schilk.philipp@gmail.com>
2024-02-16 12:03:11 +0100

    refactor: Extract core reginald functionalty to function for testing.

--- src/reginald/__main__.py
+++ src/reginald/__main__.py
@@ -4,36 +4,35 @@ from typing import List
 
 from reginald.cli import CLI, parse_args
 from reginald.error import ReginaldException
+from reginald.generator import OutputGenerator
 from reginald.input.convert_yaml import YAMLConverter
 from reginald.input.parse_yaml import YAML_RegisterMap
 from reginald.input.validate_map import MapValidator
 
 
-def main() -> int:
-    try:
-        # Parse command line args:
-        cli, generator = parse_args()
+def main():
+    # Parse command line args:
+    cli, generator = parse_args()
 
-        # Open, parse, and validate input file:
-        r = YAML_RegisterMap.from_yaml_file(cli.input_file)
-        r = YAMLConverter(r).convert()
-        MapValidator(r).validate()
+    # Generate reginald output:
+    output = generate(cli, generator)
 
-        # Generate output using selected generator:
-        output = generator.generate(r, cli.input_file, cli.output_file, cli.generator_args)
+    if cli.verify:
+        verify_file(output, cli)
+    else:
+        # Write to file:
+        with open(cli.output_file, 'w') as outfile:
+            outfile.write("\n".join(output) + "\n")
 
-        if cli.verify:
-            verify_file(output, cli)
-        else:
-            # Write to file:
-            with open(cli.output_file, 'w') as outfile:
-                outfile.write("\n".join(output) + "\n")
 
-    except ReginaldException as e:
-        print(e, file=sys.stderr)
-        return 1
+def generate(cli: CLI, generator: OutputGenerator) -> List[str]:
+    # Open, parse, and validate input file:
+    r = YAML_RegisterMap.from_yaml_file(cli.input_file)
+    r = YAMLConverter(r).convert()
+    MapValidator(r).validate()
 
-    return 0
+    # Generate output using selected generator:
+    return generator.generate(r, cli.input_file, cli.output_file, cli.generator_args)
 
 
 def verify_file(output: List[str], cli: CLI) -> int:
@@ -49,22 +48,26 @@ def verify_file(output: List[str], cli: CLI) -> int:
             file_line = file_line.rstrip("\r\n")
             output_line = output_line.rstrip("\r\n")
             if file_line != output_line:
-                print(f"Error. Existing file '{filename}' differs at line {line_idx+1}:")
-                print(f"Expected: '{output_line}'")
-                print(f"Found:    '{file_line}'")
-                return 1
+                msg = f"Error. Existing file '{filename}' differs at line {line_idx+1}:\n"
+                msg += f"Expected: '{output_line}'"
+                msg += f"Found:    '{file_line}'"
+                raise ReginaldException(msg)
 
         if len(file_lines) != len(output):
-            print(f"Error: Existing file '{filename}' has unexpected number of lines.")
-            print(f"Expected {len(output)}, is {len(file_lines)}")
-            return 1
+            msg = f"Error: Existing file '{filename}' has unexpected number of lines."
+            msg += f"Expected {len(output)}, is {len(file_lines)}"
+            raise ReginaldException(msg)
 
         return 0
 
     except FileNotFoundError:
-        print(f"Failed to open file '{cli.output_file}'.")
-        return 1
+        raise ReginaldException(f"Failed to open file '{cli.output_file}'.")
 
 
 if __name__ == '__main__':
-    exit(main())
+    try:
+        main()
+        exit(0)
+    except ReginaldException as e:
+        print(e, file=sys.stderr)
+        exit(1)
