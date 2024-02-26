0e4e1af19e16123bc4de9d596a0e70e38ff23a01
schilkp <schilk.philipp@gmail.com>
2024-02-26 18:58:51 +0100

    [WIP] pre-refactor snapshot

--- reginald_codegen/src/builtin/c/funcpack.rs
+++ reginald_codegen/src/builtin/c/funcpack.rs
@@ -679,3 +679,42 @@ fn template_has_content_to_generate(template: &Register, opts: &GeneratorOpts) -
 
     false
 }
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use std::path::PathBuf;
+
+    fn convert_yaml_example(file: &str) -> RegisterMap {
+        let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        path.push("../examples/maps/");
+        path.push(file);
+        let reader = std::fs::File::open(path).unwrap();
+        RegisterMap::from_yaml(reader).unwrap()
+    }
+
+    #[test]
+    fn snapshot_funcpack() {
+        let map = convert_yaml_example("max77654.yaml");
+        let mut out = String::new();
+        generate(
+            &mut out,
+            &map,
+            &PathBuf::from("max77654.h"),
+            &GeneratorOpts {
+                field_enum_prefix: false,
+                registers_as_bitfields: true,
+                clang_format_guard: true,
+                generate_enums: true,
+                generate_registers: true,
+                generate_register_functions: true,
+                generate_generic_macros: true,
+                generate_validation_functions: true,
+                add_include: vec![],
+            },
+        )
+        .unwrap();
+        insta::assert_snapshot!(out);
+    }
+}
